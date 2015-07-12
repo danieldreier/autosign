d967b33063204fa04152bcf1bbfaeb57f4dacb14
Daniel Dreier <d@puppetlabs.com>
2015-07-11 18:13:01 -0700

    remove unused Autosign::Api class

--- lib/autosign.rb
+++ lib/autosign.rb
@@ -1,7 +1,6 @@
 require 'autosign/version.rb'
 require 'autosign/token.rb'
 require 'autosign/config.rb'
-require 'autosign/api.rb'
 
 # Add requires for other files you add to your project here, so
 # you just need to require this one file in your bin file
